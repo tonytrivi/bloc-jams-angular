9ea4cae8f3eea5c739c9414181d4c03cf1b82562
Tony Trivison <shambles72@hotmail.com>
2016-08-03 07:28:13 -0700

    bloc jams ng completed checkpoint 5 controllers

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -9,6 +9,7 @@
         $stateProvider
             .state('landing', {
                 url: '/',
+                controller: 'LandingCtrl as landing',
                 templateUrl: '/templates/landing.html'
             })
             .state('album', {
@@ -17,8 +18,10 @@
             })
             .state('collection', {
                 url: '/collection',
+                controller: 'CollectionCtrl as collection',
                 templateUrl: '/templates/collection.html'
-            });
+ });
+
         
         
             
